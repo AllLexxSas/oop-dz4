d720f8c4d4e3a2f22d08270cb4a7895c56fd71de
AllLexxSas <wqqe82@gmail.com>
2021-10-10 21:05:04 +0300

    исправления

--- my_vector.h
+++ my_vector.h
@@ -10,7 +10,9 @@ private:
 	int array_size = 0;
 	int array_campacity = 0;
 	int* arr;
-	const int start_increment = 4;
+	int number_length = 0;//переменна€ нужна дл€ оптимицазии блочной сортировки.
+	const int increment = 4; //количесво добавл€емых €чеик при увеличении массива. ј так же стартовое количество €чеик в векторе. 
+
 
 public:
 	my_vector() = default;
@@ -22,9 +24,13 @@ public:
 
 	bool push_back(int a)
 	{
+		int _number_length = a == 0 ? 1 : static_cast<int>(log(abs(a)) + 1);
+		if (_number_length > number_length)
+			number_length = _number_length;
+
 		if (!arr) {
-			arr = new int[start_increment];
-			array_campacity = start_increment;
+			arr = new int[increment];
+			array_campacity = increment;
 		}
 		else
 		{
@@ -84,14 +90,17 @@ public:
 		int max = array_size;
 		const int _max = max;
 		int d = 10;
-		std::vector<std::vector<int>> temp_arra(d, std::vector<int>(max + 1));
+		//std::vector<std::vector<int>> temp_arra(d, std::vector<int>(max + 1));
+		int **temp_arra = new int*[d];
 
 		for (size_t i = 0; i < d; ++i)
 		{
+			temp_arra[i] = new int[_max + 1];
 			temp_arra[i][max] = 0;
 		}
+		int h = 0;
 
-		for (int j = 1; j < 1000000000; j *= 10) {
+		for (int j = 1; h <= number_length; j *= 10) {
 			for (int i = 0; i < array_size; i++)
 			{
 				int b = (arr[i] / j) % d;
@@ -109,6 +118,7 @@ public:
 				}
 				temp_arra[i][max] = 0;
 			}
+			h++;
 		}
 		return true;
 	}
@@ -134,13 +144,16 @@ void print_vector(my_vector& a)
 
 
 
-int tack1()
+int task1()
 {
 	my_vector v;
 
 	v.push_back(44);
 
+
 	v.push_back(33);
+    v.push_back(1234);
+
 	v.push_back(21);
 	v.push_back(56);
 	v.push_back(77);
--- task2.h
+++ task2.h
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <set>
 #include <vector>
-#define RAND 1 + rand() % 1000
+#include <time.h>
+
+
 
 //ƒан вектор чисел, требуетс€ вы€снить, сколько среди них различных.ѕостаратьс€ использовать максимально быстрый алгоритм.
 
@@ -10,11 +12,13 @@ void task2()
 {
 	srand(time(0));
 
+	int const rnd = 1 + rand() % 1000;
+
 	std::vector<int> a;
 
-	for (size_t i = 0; i < RAND ; i++)
+	for (size_t i = 0; i < rnd; i++)
 	{
-		a.push_back(RAND);
+		a.push_back(rnd);
 	}
 	std::set<int> s{ a.begin() , a.end() };
 	std::cout << "в векторе " << s.size() << " различных элемента" << std::endl;
